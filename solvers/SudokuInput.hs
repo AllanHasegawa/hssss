91af70672f94ee4435ed2a2dcf1f5a520bd8519f
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-11-14 01:36:48 -0200

    SudokuBoard now holds a 3D list. Not sure if its good

--- solvers/SudokuInput.hs
+++ solvers/SudokuInput.hs
@@ -1,24 +1,42 @@
 module SudokuInput
 (
+SudokuBoard,
 solveProblem
 ) where
 
 import System.IO
+import Data.Char
 
-type SudokuBoard = [[Int]]
+type SudokuBoard = [[[Maybe Int]]]
 
 solveProblem :: Int -> (SudokuBoard -> SudokuBoard) -> IO ()
 solveProblem problemIndex solver
 	| problemIndex >= 0 = (do
 		handle <- openFile "../problems/sudoku17.txt" ReadMode
 		contents <- hGetContents handle
-		print (lines contents !! problemIndex)
+		print (stringToSudokuBoard $ lines contents !! problemIndex)
 		hClose handle
 		)
 	| otherwise = print "WAT?"
 
 
+-- Transforms a string line into Sudokuboard.
+-- It should be okay for inputs with numbers <9.
 stringToSudokuBoard :: String -> SudokuBoard
-stringToSudokuBoard str = [[10]]
+stringToSudokuBoard str =	let 	mList =
+						[if x == '0' then
+							Nothing else
+							Just $ digitToInt x
+						| x <- str]
+					sizeEachInnerCube = floor . sqrt . fromIntegral . length $ str
+					sizeEachRowCol = floor . sqrt . fromIntegral $ sizeEachInnerCube
+					list2D = list1DTo2D sizeEachInnerCube mList
+					list3D = [list1DTo2D sizeEachRowCol x | x <- list2D]
+				in list3D
+
+
+list1DTo2D :: Int -> [a] -> [[a]]
+list1DTo2D _ [] = []
+list1DTo2D n l = [take n l] ++ (list1DTo2D n $ drop n l)
 
 defaultProblems = [10,500,4040,50340,30450]
