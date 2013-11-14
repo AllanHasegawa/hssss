798db7483e9a7c38ea95247648758ca5d309dc1d
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-11-14 03:11:42 -0200

    getCols implemented

--- solvers/SudokuUtils.hs
+++ solvers/SudokuUtils.hs
@@ -7,7 +7,11 @@ solveProblem
 import System.IO
 import Data.Char
 
-type SudokuBoard = [[[Maybe Int]]]
+data SudokuBoard = SudokuBoard {
+				rows :: Int,
+				rowsInSquare :: Int,
+				board :: [Maybe Int]
+			} deriving (Show)
 
 solveProblem :: Int -> (SudokuBoard -> SudokuBoard) -> IO ()
 solveProblem problemIndex solver
@@ -15,25 +19,41 @@ solveProblem problemIndex solver
 		handle <- openFile "../problems/sudoku17.txt" ReadMode
 		contents <- hGetContents handle
 		print (solver $ stringToSudokuBoard $ lines contents !! problemIndex)
+		print (getCols (stringToSudokuBoard $ lines contents !! problemIndex) 3)
 		hClose handle
 		)
 	| otherwise = print "WAT?"
 
 
+verifySolution :: SudokuBoard -> Bool
+verifySolution b = True 
+
+getCols' _ _ _ 0 = []
+getCols' b i s n = [b !! i] ++ (getCols' b (i+s) s (n-1))
+
+getCols :: SudokuBoard -> Int -> [[Maybe Int]]
+getCols s g = let
+			r = rows s
+			rS = rowsInSquare s
+			pInitial = ((g `mod` rS) * rS) + ((g `div` rS) * r * rS)
+			steps = rows s
+			b = board s
+		 in [getCols' b (pInitial+n) steps rS | n <- [0..2]]
+
+
 -- Transforms a string line into Sudokuboard.
 -- It should be okay for inputs with numbers <9.
 stringToSudokuBoard :: String -> SudokuBoard
-stringToSudokuBoard str =	let 	mList =
-						[if x == '0' then
-							Nothing else
-							Just $ digitToInt x
-						| x <- str]
-					sizeEachInnerCube = floor . sqrt . fromIntegral . length $ str
-					sizeEachRowCol = floor . sqrt . fromIntegral $ sizeEachInnerCube
-					list2D = list1DTo2D sizeEachInnerCube mList
-					list3D = [list1DTo2D sizeEachRowCol x | x <- list2D]
-				in list3D
-
+stringToSudokuBoard str = let
+				sizeGrid = floor . sqrt . fromIntegral . length $ str
+				sizeRowsCols = floor . sqrt . fromIntegral $ sizeGrid
+				nList = [
+					if x == '0' then
+						Nothing
+					else
+						Just $ digitToInt x
+					| x <- str]
+	         	  in SudokuBoard sizeGrid sizeRowsCols nList 
 
 list1DTo2D :: Int -> [a] -> [[a]]
 list1DTo2D _ [] = []
