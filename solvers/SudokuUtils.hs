4e5e9e7cc0a5f84d815014b8cf96fb5f9876940e
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-11-26 22:03:57 -0200

    modified board units; implemented getRow;

--- solvers/SudokuUtils.hs
+++ solvers/SudokuUtils.hs
@@ -7,10 +7,15 @@ solveProblem
 import System.IO
 import Data.Char
 
+data SudokuUnit = Empty | Hint Int | Guess Int deriving (Show)
+
+-- sS :: size of the small square
+-- bS :: size of the big square
+-- board :: list of SudokuUnit's
 data SudokuBoard = SudokuBoard {
-				rows :: Int,
-				rowsInSquare :: Int,
-				board :: [Maybe Int]
+				sS :: Int,
+				bS :: Int,
+				board :: [SudokuUnit]
 			} deriving (Show)
 
 solveProblem :: Int -> (SudokuBoard -> SudokuBoard) -> IO ()
@@ -19,7 +24,7 @@ solveProblem problemIndex solver
 		handle <- openFile "../problems/sudoku17.txt" ReadMode
 		contents <- hGetContents handle
 		print (solver $ stringToSudokuBoard $ lines contents !! problemIndex)
-		print (getCols (stringToSudokuBoard $ lines contents !! problemIndex) 3)
+		print (getRowBS (stringToSudokuBoard $ lines contents !! problemIndex) 6)
 		hClose handle
 		)
 	| otherwise = print "WAT?"
@@ -28,35 +33,53 @@ solveProblem problemIndex solver
 verifySolution :: SudokuBoard -> Bool
 verifySolution b = True 
 
-getCols' _ _ _ 0 = []
-getCols' b i s n = [b !! i] ++ (getCols' b (i+s) s (n-1))
+-- get row (big square)
+getRowBS sBoard rowIndex
+		| rowIndex < 0 = [Empty]
+		| rowIndex >= bS sBoard = [Empty]
+		| otherwise = let
+				sBS = bS sBoard
+				sSS = sS sBoard
+				b = board sBoard
+				pInitial = rowIndex * sBS
+		 	in slice pInitial (pInitial+sBS) 1 b
 
-getCols :: SudokuBoard -> Int -> [[Maybe Int]]
-getCols s g = let
-			r = rows s
-			rS = rowsInSquare s
-			pInitial = ((g `mod` rS) * rS) + ((g `div` rS) * r * rS)
-			steps = rows s
-			b = board s
-		 in [getCols' b (pInitial+n) steps rS | n <- [0..2]]
+-- get column (big square)
+getColBS :: SudokuBoard -> Int -> [SudokuUnit]
+getColBS sBoard colIndex
+		| colIndex < 0 = [Empty]
+		| colIndex >= bS sBoard = [Empty]
+		| otherwise = let
+				sBS = bS sBoard
+				sSS = sS sBoard
+				b = board sBoard
+				steps = bS sBoard
+				pInitial = colIndex
+		 	in slice pInitial (length $ b) steps b
 
 
 -- Transforms a string line into Sudokuboard.
 -- It should be okay for inputs with numbers <9.
 stringToSudokuBoard :: String -> SudokuBoard
 stringToSudokuBoard str = let
-				sizeGrid = floor . sqrt . fromIntegral . length $ str
-				sizeRowsCols = floor . sqrt . fromIntegral $ sizeGrid
+				sizeBS = floor . sqrt . fromIntegral . length $ str
+				sizeSS = floor . sqrt . fromIntegral $ sizeBS
 				nList = [
 					if x == '0' then
-						Nothing
+						Empty
 					else
-						Just $ digitToInt x
+						Hint $ digitToInt x
 					| x <- str]
-	         	  in SudokuBoard sizeGrid sizeRowsCols nList 
+	         	  in SudokuBoard sizeSS sizeBS nList 
 
+-- Aux functions
 list1DTo2D :: Int -> [a] -> [[a]]
 list1DTo2D _ [] = []
 list1DTo2D n l = [take n l] ++ (list1DTo2D n $ drop n l)
+
+slice :: Int -> Int -> Int -> [a] -> [a]
+slice from to step list
+	| from >= to = []
+	| otherwise = [(list !! from)] ++ (slice (from+step) to step list)
 
 defaultProblems = [10,500,4040,50340,30450]
