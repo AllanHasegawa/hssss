028f7f44d5f294a0574f8f012fd71cbe9d3379fd
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-11-28 01:44:26 -0200

    initial version

--- solvers/HasegawaAranSolver.hs
+++ solvers/HasegawaAranSolver.hs
@@ -1,8 +1,39 @@
+import Data.List
 import SudokuUtils as SU
 
-myf :: SudokuBoard -> SudokuBoard
-myf f = f
-
 main = do
-	SU.solveProblem 44900 myf
+	SU.solveProblem 0 mySolver
 	-- print $ head problem 
+
+mySolver :: SudokuBoard -> SudokuBoard
+mySolver sBoard = sBoard
+
+-- solve (Empty):xs 
+
+genGuesses :: SudokuBoard -> Int -> SudokuUnit
+genGuesses sBoard index = let
+	sBS = bS sBoard
+	u = getUniques sBoard index
+	aGuesses = [1..sBS]
+	in Guess [x | x <- aGuesses, (Hint x) `notElem` u]
+
+getUniques :: SudokuBoard -> Int -> [SudokuUnit]
+getUniques sBoard index = let
+	uSS = uniqueSudokuUnits $ getSS (iBStoSS sBoard index) sBoard
+	uCol = uniqueSudokuUnits $ getColBS (iBStoCol sBoard index) sBoard
+	uRow = uniqueSudokuUnits $ getRowBS (iBStoRow sBoard index) sBoard
+	in nub $ uSS `union` uCol `union` uRow
+
+iBStoCol :: SudokuBoard -> Int -> Int
+iBStoCol sBoard index = index `mod` (bS sBoard)
+
+iBStoRow :: SudokuBoard -> Int -> Int
+iBStoRow sBoard index = index `div` (bS sBoard)
+
+iBStoSS :: SudokuBoard -> Int -> Int
+iBStoSS sBoard index = let
+	c = iBStoCol sBoard index
+	r = iBStoRow sBoard index
+	s = bS sBoard
+	i = sS sBoard
+	in ((r `div` i) * i) + (c `div` i)
