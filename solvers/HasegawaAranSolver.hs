444c57f448d66da111973d3f6258d57f9ec18dbf
Yoshio Hasegawa <hasegawa.aran@gmail.com>
2013-11-28 20:00:59 -0200

    dont know what im doing

--- solvers/HasegawaAranSolver.hs
+++ solvers/HasegawaAranSolver.hs
@@ -1,14 +1,45 @@
 import Data.List
 import SudokuUtils as SU
+import Debug.Trace
 
 main = do
 	SU.solveProblem 0 mySolver
 	-- print $ head problem 
 
 mySolver :: SudokuBoard -> SudokuBoard
-mySolver sBoard = sBoard
+mySolver sBoard = let
+	sBS = bS sBoard
+	in solve sBoard sBoard False 0 (sBS*sBS)
 
--- solve (Empty):xs 
+lenGuesses (Guess x) = length x
+remGuess (Guess (x:xs)) = Guess xs
+
+solve oBoard uBoard False n end | trace ("solve " ++ (show n)) False = undefined
+
+solve oBoard uBoard backing n end
+	| n == end = uBoard
+	| isEmpty x = let
+		g = genGuesses uBoard n
+		l = lenGuesses g
+		backBoard = drop (n+1) $ board oBoard
+		u = updateBoard uBoard n (g:backBoard)
+		in if l > 0 then
+      			solve oBoard u False (n+1) end
+	   	   else
+			solve oBoard uBoard True (n-1) end 
+	| isHint x = if backing then
+			 solve oBoard uBoard True (n-1) end
+		     else
+			 solve oBoard uBoard False (n+1) end
+	| isGuess x = let
+		l = lenGuesses x
+		backBoard = drop (n+1) $ board oBoard
+		u = updateBoard uBoard n ((remGuess x):backBoard)
+		in if l > 1 then
+	      		solve oBoard u False (n+1) end
+		   else
+	      		solve oBoard uBoard True (n-1) end
+	where x = (board uBoard) !! n
 
 genGuesses :: SudokuBoard -> Int -> SudokuUnit
 genGuesses sBoard index = let
